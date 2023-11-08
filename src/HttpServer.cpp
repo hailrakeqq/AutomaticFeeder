3745cc8cecba6c59d7837e766edf12dc06963924
Yurij Boklan <hailrake@riseup.net>
2023-11-08 16:34:48 +0200

    update

--- src/HttpServer.cpp
+++ src/HttpServer.cpp
@@ -1,71 +1,64 @@
 #include "../include/HttpServer.h"
 #include "../include/StepperMotor.h"
 
-HttpServer::HttpServer(): server(80){}
+HttpServer::HttpServer() : server(80) {}
 
-void HttpServer::begin(const char* ssid, const char* password, StepperMotor *stepperMotor){
-    WiFi.begin(ssid, password);
-    while(WiFi.status() != WL_CONNECTED){
-        delay(1000);
-        Serial.print('.');
-    }
-    Serial.println("\nWiFi connected");
-    Serial.println("\nServer Address: " + WiFi.localIP().toString());
+void HttpServer::begin(const char *ssid, const char *password,
+                       StepperMotor *stepperMotor) {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print('.');
+  }
+  Serial.println("\nWiFi connected");
+  Serial.println("\nServer Address: " + WiFi.localIP().toString());
 
-    server.on("/", [this](){
-        this->handleRoot();
-    });
-    
-    server.on("/feed", [this, stepperMotor](){
-        this->handleFeed(stepperMotor);
-    });
+  server.on("/", [this]() { this->handleRoot(); });
 
-    server.on("/change_feedamount", HTTP_POST, [this](){
-        this->handleChangeFeedAmount();
-    });
+  server.on("/feed",
+            [this, stepperMotor]() { this->handleFeed(stepperMotor); });
 
-    server.on("/get_feedamount", [this](){
-        this->handleGetFeedAmount();
-    });
+  server.on("/change_feedamount", HTTP_POST,
+            [this]() { this->handleChangeFeedAmount(); });
 
-    server.begin();
-    Serial.println("HTTP server has been started");
+  server.on("/get_feedamount", [this]() { this->handleGetFeedAmount(); });
+
+  server.begin();
+  Serial.println("HTTP server has been started");
 }
 
-void HttpServer::handleFeed(StepperMotor* stepperMotor){
-    stepperMotor->feed();
-    server.send(200, "text/plain", "feed working.");
+void HttpServer::handleFeed(StepperMotor *stepperMotor) {
+  stepperMotor->feed();
+  server.send(200, "text/plain", "feed working.");
 }
 
-void HttpServer::handleChangeFeedAmount(){
-      if (server.method() == HTTP_POST) {
-        if (server.hasArg("plain")) {
-            int value = server.arg("plain").toInt();
-            
-            EEPROM.put(1, value);
-            EEPROM.commit();
-                  
-            server.send(200, "text/plain", "Feed Amount was successfully updated");
-        } else {
-            server.send(400, "text/plain", "Bad Request - Missing JSON data");
-        }
+void HttpServer::handleChangeFeedAmount() {
+  if (server.method() == HTTP_POST) {
+    if (server.hasArg("plain")) {
+      int value = server.arg("plain").toInt();
+
+      EEPROM.put(1, value);
+      EEPROM.commit();
+
+      server.send(200, "text/plain", "Feed Amount was successfully updated");
     } else {
-        server.send(405, "text/plain", "Method Not Allowed");
+      server.send(400, "text/plain", "Bad Request - Missing JSON data");
     }
+  } else {
+    server.send(405, "text/plain", "Method Not Allowed");
+  }
 }
 
-void HttpServer::handleGetFeedAmount(){
-    int feedAmount;
+void HttpServer::handleGetFeedAmount() {
+  int feedAmount;
 
-    EEPROM.get(1, feedAmount);
-    String result = String(feedAmount);
-    server.send(200, "text/plain", result);
+  EEPROM.get(1, feedAmount);
+  String result = String(feedAmount);
+  server.send(200, "text/plain", result);
 }
 
-void HttpServer::handleRequest(){
-    server.handleClient();
-}
+void HttpServer::handleRequest() { server.handleClient(); }
 
-void HttpServer::handleRoot(){
-    server.send(200, "text/plain", "Hello from NodeMCU!!!");
+void HttpServer::handleRoot() {
+  server.send(200, "text/plain", "Hello from NodeMCU!!!");
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,67 @@
-#include <Arduino.h>
-#include <EEPROM.h>
-#include "ButtonDecoder.cpp"
 #include "../include/HttpServer.h"
 #include "../include/StepperMotor.h"
 #include "../include/secret.h"
+#include "ButtonDecoder.cpp"
+#include <Arduino.h>
+#include <EEPROM.h>
 
 #define EE_RESET 12
-#define STEPS_FORWARD 19
-#define STEPS_BACK 12
+#define STEPS_FORWARD 25
+#define STEPS_BACK 20
 #define FEED_SPEED 3000 // delay between motor steps(ms)
 #define NUMBER_OF_PHASE 4
-#define BUTTON_PIN 14 // nodemcu D5
-// 28BYJ48 pin 1 | nodemcu D0 == 16
-// 28BYJ48 pin 2 | nodemcu D1 == 5
-// 28BYJ48 pin 3 | nodemcu D2 == 4
-// 28BYJ48 pin 4 | nodemcu D4	== 2
-int8_t motorPins[] = {16, 5, 4, 2};
+#define BUTTON_PIN D5
 
-const int mechChangeBtn = D7; // Номер пина, к которому подключена кнопка
-const int led = D6;       // Номер пина, к которому подключен светодиод
+int8_t motorPins[] = {D0, D1, D2, D3};
+
+const int mechChangeBtn = D7;
+const int led = D6;
 
 int sensorValue = 0;
 int outputValue = 0;
 int lastOutputValue = 0;
 boolean mechChangeBtnState = false;
 boolean mechChangeBtnLastState = false;
-boolean isMechChangeFeedAmountRunning = false; // Переменная для отслеживания состояния выполнения кода
-
+boolean isMechChangeFeedAmountRunning = false;
 
 int feedAmount = 100;
 
-StepperMotor stepperMotor(motorPins, FEED_SPEED, STEPS_BACK, STEPS_FORWARD, feedAmount);
+StepperMotor stepperMotor(motorPins, FEED_SPEED, STEPS_BACK, STEPS_FORWARD,
+                          feedAmount);
 HttpServer httpServer;
 
 void setup() {
-  Serial.begin(115200); 
+  Serial.begin(115200);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(mechChangeBtn, INPUT_PULLUP);
 
   EEPROM.begin(4096); // it's should be here if you use esp8266 or esp32
-  if(EEPROM.read(0) != EE_RESET) {
+  if (EEPROM.read(0) != EE_RESET) {
     EEPROM.write(0, EE_RESET);
     EEPROM.put(1, feedAmount);
     EEPROM.commit(); // EEPROM.commit() uses only for esp8266 or esp32
   }
- 
-  httpServer.begin(ssid, password, &stepperMotor);
+
+  httpServer.begin(SSID, PASSWORD, &stepperMotor);
 }
 
-void loop(){
+void loop() {
   httpServer.handleRequest();
 
-  if(digitalRead(BUTTON_PIN) == LOW && isMechChangeFeedAmountRunning == false)//TODO: create function for check is button was clicked or holded
+  if (digitalRead(BUTTON_PIN) == LOW && isMechChangeFeedAmountRunning == false)
     stepperMotor.feed();
 
   mechChangeBtnState = digitalRead(mechChangeBtn);
 
   if (mechChangeBtnState == LOW && mechChangeBtnLastState == HIGH) {
     if (isMechChangeFeedAmountRunning) {
-      //Выключаем
       isMechChangeFeedAmountRunning = false;
       analogWrite(led, 0);
       Serial.print("Last Output Value = ");
       Serial.println(lastOutputValue);
       EEPROM.put(1, lastOutputValue);
       EEPROM.commit();
-    } else { 
+    } else {
       isMechChangeFeedAmountRunning = true;
     }
   }
